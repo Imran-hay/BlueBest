8858f3851331d45a190e0a65fb4fdf640dec3c39
alecan27 <alazar.damena01@gmail.com>
2023-07-08 02:27:47 -0700

    dnvoed

--- scripts/observer.js
+++ scripts/observer.js
@@ -2,7 +2,16 @@
 const target = document.querySelector('.animated-text');
 
 const options = {
+
+    threshold: 0.5,
+  }
+
+  const aoptions = {
     threshold: 0.8,
+  }
+
+  const options1 = {
+    threshold: 0.2,
   }
 
 // Next we want to create a function that will be called when that element is intersected
@@ -23,3 +32,76 @@ const observer = new IntersectionObserver(handleIntersection,options);
 
 // Finally start observing the target element
 observer.observe(target);
+
+
+const target1 = document.querySelectorAll('.text-non');
+
+
+// Next we want to create a function that will be called when that element is intersected
+function handleIntersection1(entries) {
+// The callback will return an array of entries, even if you are only observing a single item
+entries.forEach((entry) => {
+  if (entry.isIntersecting) {
+    entry.target.classList.add('text-opa')
+  } else {
+    entry.target.classList.remove('text-opa')
+  }
+});
+}
+
+// Next we instantiate the observer with the function we created above. This takes an optional configuration
+// object that we will use in the other examples.
+const observer1 = new IntersectionObserver(handleIntersection1,options1);
+
+// Finally start observing the target element
+target1.forEach(function (i) {
+ 
+      observer1.observe(i);
+  
+});
+
+
+
+
+
+/* home */
+
+const htarget = document.querySelectorAll('.home');
+function handleIntersection2(entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      document.getElementById("home-active").classList.add('active-bg');
+    } else {
+      document.getElementById("home-active").classList.remove('active-bg');
+    }
+  });
+}
+const hobserver = new IntersectionObserver(handleIntersection2,aoptions);
+htarget.forEach(function (i) {
+ 
+  hobserver.observe(i);
+
+});
+/* home end */
+
+
+
+/* about us */
+
+const atarget = document.querySelectorAll('.about-us');
+function handleIntersection3(entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      document.getElementById("about-active").classList.add('active-bg');
+    } else {
+      document.getElementById("about-active").classList.remove('active-bg');
+    }
+  });
+}
+const aobserver = new IntersectionObserver(handleIntersection3,aoptions);
+atarget.forEach(function (i) {
+ 
+  aobserver.observe(i);
+
+});
+/* about us */
